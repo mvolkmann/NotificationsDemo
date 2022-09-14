30ac92d3eec00185e2a141d842b5e63595ccea6f
R. Mark Volkmann <r.mark.volkmann@gmail.com>
2022-09-14 14:08:29 -0500

    Initial Commit

---
+++ NotificationsDemo/NotificationsDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  NotificationsDemoApp.swift
+//  NotificationsDemo
+//
+//  Created by Mark Volkmann on 9/14/22.
+//
+
+import SwiftUI
+
+@main
+struct NotificationsDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
